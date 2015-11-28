8cc14ad12c1b38a66f74fab79c3d6c3e9e34aa82
m7mdyahia <moda.yahia@gmail.com>
2015-11-28 16:00:32 +0200

    Message inheritance

--- Chat/src/chatapp/Message.java
+++ Chat/src/chatapp/Message.java
@@ -1,8 +1,10 @@
 package chatapp;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author Amr
+ * @author Mohammad Yahia
  */
 public class Message {
     
@@ -12,15 +14,43 @@ public class Message {
         Create_Group,
         List_Groups,
         Join_Group,
-        Conv_Msg
+        Conv_Msg,
+        List_Users
     }
-    
-    public MsgType msg;
+    public Message() {
+		super();
+    }
+    public Message(MsgType msg, String data) {
+		super();
+		this.msg = msg;
+		this.data = data;
+	}
+	public MsgType msg;
     public String data;
-    public online_user ouser ;
-    
     public void dataTosend(MsgType m,String d){
         this.msg= m;
         this.data= d;
     }
+}
+
+class ListofUseres extends Message
+{
+	
+	public ListofUseres(String data,ArrayList<User> userlist) {
+		super( MsgType.List_Users, data);
+		this.userlist = userlist;
+	}
+
+	ArrayList<User> userlist;
+}
+
+class ListofGroups extends Message
+{
+	
+	public ListofGroups(String data,ArrayList<available_groups> grouplist) {
+		super( MsgType.List_Groups, data);
+		this.grouplist = grouplist;
+	}
+
+	ArrayList<available_groups> grouplist;
 }
